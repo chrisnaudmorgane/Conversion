553c6569f3a3374e7f219db301bd30b6815a24d2
chrisnaudmorgane <chrisnaud.agossou@epitech.eu>
2024-08-22 12:02:22 +0100

    customize the header

--- convertion_app/src/app/design.js
+++ convertion_app/src/app/design.js
@@ -1,15 +1,13 @@
 export default function Header() {
-    return (
-      <header className="bg-white text-black py-5 shadow-md">
-        <div className="flex justify-between items-center px-10">
-          <div className="text-2xl font-semibold">
-            Made By <span className="text-orange-700">AGOSSOU</span> Chrisnaud
-          </div>
-          <div className="text-lime-400 bg-rose-800 w-48 h-14 flex items-center justify-center rounded-md">
-            Json-Csv Conversion
-          </div>
+  return (
+    <header className="bg-gray-800 text-white py-4 shadow-lg">
+      <div className="container mx-auto text-center">
+        <div className="text-xl font-bold">
+          <span className="text-orange-400">Made By</span> 
+          <span className="text-orange-500 font-semibold"> AGOSSOU</span> 
+          <span className="text-orange-400"> Chrisnaud</span>
         </div>
-      </header>
-    );
-  }
-  
+      </div>
+    </header>
+  );
+}
